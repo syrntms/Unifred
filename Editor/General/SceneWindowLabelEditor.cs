1915d9d357f40f6e40e5231cf1028a002bf815a9
Noriyoshi Samata <noriyoshi.samata@G-PC-6581.local>
2015-11-30 10:37:20 +0900

    refactoring scene window label editor

--- Editor/General/SceneWindowLabelEditor.cs
+++ Editor/General/SceneWindowLabelEditor.cs
@@ -8,18 +8,7 @@ namespace Unifred
 	public class SceneWindowLabelEditor : Editor
 	{
 
-		private static GUIStyle style = new GUIStyle()
-		{
-			normal = new GUIStyleState() {
-				background = TextureUtility.MakeFrameTexture(Color.gray, new Color32(0, 0, 0, 0x3F)),
-				textColor = new Color32(0xCC, 0x66, 0x00, 0xFF),
-			},
-			border = new RectOffset(7, 8, 7, 8),
-			padding = new RectOffset(5, 5, 5, 5),
-			margin = new RectOffset(0, 0, 0, 0),
-			contentOffset = new Vector2(3, 3),
-			richText = true,
-		};
+		private static GUIStyle style;
 
 		[DrawGizmo(GizmoType.Pickable | GizmoType.NonSelected | GizmoType.Selected)]
 		public static void OnDrawGizmo(SceneWindowLabel sceneWindowLabel, GizmoType type)
@@ -29,9 +18,29 @@ namespace Unifred
 				return;
 			}
 
+			if (style == null) {
+				style = _CreateStyle();
+			}
+
 			Handles.BeginGUI();
 			Handles.Label(sceneWindowLabel.transform.position, label, style);
 			Handles.EndGUI();
+		}
+
+		private static GUIStyle _CreateStyle()
+		{
+			return new GUIStyle() {
+				normal =
+				new GUIStyleState() {
+					background	= TextureUtility.MakeFrameTexture(Color.gray, new Color32(0, 0, 0, 0x3F)),
+					textColor	= new Color32(0xCC, 0x66, 0x00, 0xFF),
+				},
+				border			= new RectOffset(7, 8, 7, 8),
+				padding			= new RectOffset(5, 5, 5, 5),
+				margin			= new RectOffset(0, 0, 0, 0),
+				contentOffset	= new Vector2(3, 3),
+				richText		= true,
+			};
 		}
 	}
 }
