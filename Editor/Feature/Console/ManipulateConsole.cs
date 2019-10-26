a522a15120d7cd2f5a7ed807f17f5f32bc4e40dc
syrntms <syrntms@gmail.com>
2019-10-26 14:12:12 +0900

    Update ManipulateConsole.cs
    update clear console feature

--- Editor/Feature/Console/ManipulateConsole.cs
+++ Editor/Feature/Console/ManipulateConsole.cs
@@ -8,7 +8,7 @@ namespace Unifred.Feature
 	{
 		public static void Clear()
 		{
-			var logEntries = System.Type.GetType("UnityEditorInternal.LogEntries,UnityEditor.dll");
+			var logEntries = System.Type.GetType("UnityEditor.LogEntries,UnityEditor.dll");
 			var clearMethod = logEntries.GetMethod("Clear", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
 			clearMethod.Invoke(null, null);
 		}
